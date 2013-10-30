9bde6fe3b52532ffeb4ac53c803c714262aa17a8
tanshbo <1244048470@qq.com>
2013-10-30 23:35:00 +0800

    add css

--- template/sidebar.js
+++ template/sidebar.js
@@ -1,7 +1,8 @@
 var sidebar_template={};
 sidebar_template.recentPost={'html':'\
 <div class="sidebar_recent_post"> \
-<% _.each(recentPosts, function(name) { %> <li><%= name %></li> <% }); %> \
+<h1>Recent Post</h1>\
+<ul><% _.each(recentPosts, function(name) { %> <li><%= name %></li> <% }); %> </ul>\
 </div> \
 '};
 sidebar_template.twitter={'html':'\
